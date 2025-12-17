fbb7d0c26c233e53dcf6a4997b134f6bad271a49
Jasur Yuldashev <yuldashevj@gmail.com>
2025-12-17 12:47:24 +0500

    feat: implement SEND_UART state in application FSM

--- firmware/src/app/app_fsm.c
+++ firmware/src/app/app_fsm.c
@@ -1,16 +1,17 @@
 #include "app_fsm.h"
 
-#include "../drivers/sensors/ds18b20.h"
+#include "drivers/sensors/ds18b20.h"
 #include "drivers/sensors/mpu6050.h"
+#include "drivers/comm/uart.h"
 
 #include <util/delay.h>
 #include <stdint.h>
 
-/* ===== внутреннее состояние FSM ===== */
+/* ===== FSM internal state ===== */
 
 static app_state_t current_state;
 
-/* данные датчиков (static → живут между вызовами) */
+/* Cached sensor data */
 static int16_t temp_raw;
 static int16_t acc_x;
 static int16_t acc_y;
@@ -22,12 +23,11 @@ void app_fsm_init(void) {
     current_state = APP_STATE_INIT;
 }
 
-/* один шаг FSM */
 void app_fsm_step(void) {
     switch (current_state) {
 
     case APP_STATE_INIT:
-        /* здесь только инициализация логики */
+        /* Initialize application-level modules */
         ds18b20_init();
         mpu6050_init();
 
@@ -35,6 +35,7 @@ void app_fsm_step(void) {
         break;
 
     case APP_STATE_READ_SENSORS:
+         /* Read sensors */
         temp_raw = ds18b20_read_raw();
         mpu6050_read_accel(&acc_x, &acc_y, &acc_z);
 
@@ -42,14 +43,25 @@ void app_fsm_step(void) {
         break;
 
     case APP_STATE_SEND_UART:
-        /* ⚠️ пока UART остаётся в main через функцию */
-        /* здесь только подготовка данных */
+        /* Send data over UART */
+        uart_print("T=");
+        uart_print_int(temp_raw / 16);
+        uart_print("C ");
+
+        uart_print("AX=");
+        uart_print_int(acc_x);
+        uart_print(" AY=");
+        uart_print_int(acc_y);
+        uart_print(" AZ=");
+        uart_print_int(acc_z);
+
+        uart_print("\r\n");
 
         current_state = APP_STATE_SLEEP;
         break;
 
     case APP_STATE_SLEEP:
-        /* пока просто задержка, позже будет sleep_cpu() */
+        /* Temporary blocking delay (will be replaced by sleep + watchdog) */
         _delay_ms(2000);
 
         current_state = APP_STATE_READ_SENSORS;
